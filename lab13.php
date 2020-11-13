f743ae81afbfe03ea248394f4920f4914c5eda4d
63070134 <73682774+63070134@users.noreply.github.com>
2020-11-13 12:01:42 +0700

    Update lab13.php

--- lab13.php
+++ lab13.php
@@ -5,7 +5,7 @@
 <body>
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'lab13.mysql.database.azure.com', 'panupong@lab13', 'Boom123456', 'itflab', 3306);
+mysqli_real_connect($conn, 'lab13.mysql.database.azure.com', 'panupong@lab13', 'Boom123456', 'IFTlab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
