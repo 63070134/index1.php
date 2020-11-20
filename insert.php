c18a57d102580cb3a9746ff78ad8f1fa3df4cd60
63070134 <73682774+63070134@users.noreply.github.com>
2020-11-20 10:39:52 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,16 +1,15 @@
 <?php
 
-$conn = mysqli_init();
-mysqli_real_connect($conn, 'lab13.mysql.database.azure.com', 'panupong@lab13', 'Boom123456', 'itflab', 3306);
+$conn = mysqli_connect('63070054itf.mysql.database.azure.com', 'waveza2@63070054itf', 'Zaza456654', 'ITFlab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
 
-$name = $_POST['name'];
-$comment = $_POST['comment'];
-$link = $_POST['link'];
+$name = $_POST['Name'];
+$comment = $_POST['Comment'];
+$link = $_POST['Link'];
 
 
 $sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment', '$link')";
@@ -18,9 +17,26 @@ $sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment
 
 if (mysqli_query($conn, $sql)) {
     echo "New record created successfully";
-  } else {
+  } 
+  else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
+
   
 mysqli_close($conn);
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Insert</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <br><a href="show.php" class="btn btn-primary">BACK</a>
+</body>
+</html>
